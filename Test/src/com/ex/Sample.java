f579de2dc6bc05a5175322b20174fc0d4bad2dd5
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-01-26 11:09:41 +0530

    added line by remote

--- Test/src/com/ex/Sample.java
+++ Test/src/com/ex/Sample.java
@@ -2,7 +2,8 @@ package com.ex;
 
 public class Sample {
 	public static void main(String[] args) {
-		System.out.println("1st upload");
+		System.out.println("1st local repository");
+		System.out.println("second remote global master repository");
 	}
 
 }
